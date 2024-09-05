757a3b33990421c289e56870439fd1e086ab8b31
kalpan-shah <kalpanshah106@gmail.com>
2024-09-05 19:49:52 +0530

    WIP: Added and modified functions to add/delete/get and modify the data

--- server/main.py
+++ server/main.py
@@ -2,7 +2,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-posts = []
+posts = [
+    {"userId": 1, "id": 1, "title": "this is the title", "body": "this is the content"}
+]
 
 
 @app.route('/')
@@ -12,21 +14,26 @@ def index():
 
 @app.route('/post/add', methods=['POST'])
 def add_post():
-    posts.append(request.get_json())
+    d_dict = request.get_json()
+    if d_dict["id"] in [post["id"] for post in posts]:
+        return 'already exists', 400
+    print(d_dict)
+    posts.append(d_dict)
     return 'got the post request'
 
 
-@app.route('/posts', methods=['GET'])
+@app.route('/post', methods=['GET'])
 def get_post():
+    print(request.get_data())
     _id = request.get_json()["id"]
     for post in posts:
         if post['id'] == _id:
             return post
 
-    return 'not found'
+    return 'not found', 404
 
 
-@app.route('/posts', methods=['DELETE'])
+@app.route('/post/delete', methods=['DELETE'])
 def del_post():
     _to_be_del = None
     _id = request.get_json()["id"]
@@ -37,8 +44,26 @@ def del_post():
     if _to_be_del is None:
         return 'not found'
     else:
+        print("DEL: ", _to_be_del)
         posts.remove(_to_be_del)
         return 'deleted'
+
+
+@app.route('/post/edit', methods=['PUT'])
+def edit_post():
+    _to_be_updated = None
+    _id = request.get_json()["id"]
+    for post in posts:
+        if post['id'] == _id:
+            _to_be_updated = post
+            break
+    if _to_be_updated is None:
+        return 'not found', 404
+    else:
+        _index = posts.index(_to_be_updated)
+        posts[_index] = request.get_json()
+
+        return 'updated'
 
 
 app.run(debug=True)
